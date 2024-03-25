dadb970263b038538b0bd26841b864c9015486a2
Russell Dale <russgm1@gmail.com>
2024-03-25 15:12:48 -0500

    new

---
+++ Rad2/Models/OfficeAssignmentRepository.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rad2.Models.Domian
+{
+    public class OfficeAssignmentRepository : SqlRepository<OfficeAssignment>, IOfficeAssignmentRepository
+    {
+        public OfficeAssignmentRepository(dbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<OfficeAssignment> GetAll()
+        {
+            return EfDbSet
+                .Include("Instructor")
+                ;
+        }
+
+        public override async Task<OfficeAssignment> GetById(object id)
+        {
+            return await GetAll().SingleOrDefaultAsync(c => c.InstructorId == (int)id);
+        }
+
+        public IEnumerable<OfficeAssignment> GetForOfficeAssignment(int id)
+        {
+            return GetAll().Where(o => o.InstructorId == id).ToList();
+        }
+
+        public async Task Insert(OfficeAssignment OfficeAssignment)
+        {
+            await EfDbSet.AddAsync(OfficeAssignment);
+        }
+
+        public async Task Update(OfficeAssignment OfficeAssignment)
+        {
+            var entry = Context.Entry(OfficeAssignment);
+            if (entry.State == EntityState.Detached)
+            {
+                var attachedOfficeAssignment = await GetById(OfficeAssignment.InstructorId);
+                if (attachedOfficeAssignment != null)
+                {
+                    Context.Entry(attachedOfficeAssignment).CurrentValues.SetValues(OfficeAssignment);
+                }
+                else
+                {
+                    entry.State = EntityState.Modified;
+                }
+            }
+        }
+
+        public void Delete(OfficeAssignment OfficeAssignment)
+        {
+            EfDbSet.Remove(OfficeAssignment);
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+    }
+
+    public interface IOfficeAssignmentRepository
+    {
+        Task Insert(OfficeAssignment OfficeAssignment);
+        Task Update(OfficeAssignment OfficeAssignment);
+        void Delete(OfficeAssignment OfficeAssignment);
+        void Save();
+    }
+}
---
+++ Rad2/Models/TitlesRepository.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rad2.Models.Domian
+{
+    public class TitlesRepository : SqlRepository<Titles>, ITitlesRepository
+    {
+        public TitlesRepository(dbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<Titles> GetAll()
+        {
+            return EfDbSet
+                ;
+        }
+
+        public override async Task<Titles> GetById(object id)
+        {
+            return await GetAll().SingleOrDefaultAsync(c => c.Id == (int)id);
+        }
+
+        public IEnumerable<Titles> GetForTitles(int id)
+        {
+            return GetAll().Where(o => o.Id == id).ToList();
+        }
+
+
+        public IEnumerable<Titles> GetForTitles2(int Id)
+        {
+            return GetAll().Where(o => o.Id2 == Id).ToList();
+        }
+
+        public async Task Insert(Titles titles)
+        {
+            await EfDbSet.AddAsync(titles);
+        }
+
+        public async Task Update(Titles titles)
+        {
+            var entry = Context.Entry(titles);
+            if (entry.State == EntityState.Detached)
+            {
+                var attachedTitles = await GetById(titles.Id);
+                if (attachedTitles != null)
+                {
+                    Context.Entry(attachedTitles).CurrentValues.SetValues(titles);
+                }
+                else
+                {
+                    entry.State = EntityState.Modified;
+                }
+            }
+        }
+
+        public void Delete(Titles titles)
+        {
+            EfDbSet.Remove(titles);
+        }
+
+        public void Save()
+        {
+            Context.SaveChanges();
+        }
+    }
+
+    public interface ITitlesRepository
+    {
+        Task Insert(Titles titles);
+        Task Update(Titles titles);
+        void Delete(Titles titles);
+        void Save();
+    }
+}
