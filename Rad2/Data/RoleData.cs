fb2dcb3ea32101127220a89d0b7e7715b9504253
Russell Dale <russgm1@gmail.com>
2024-04-16 17:06:35 -0500

    new

--- Rad2/Data/RoleData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Rad2.Data
-{
-    public class RoleData
-    {
-        public string RoleName { get; set; } = string.Empty;
-        public string ID { get; set; } = string.Empty;
-    }
-}
--- Rad2/Data/UserData.cs
+++ Rad2/Data/UserData.cs
@@ -4,10 +4,6 @@ namespace Rad2.Data
 {
     public class UserData
     {
-        [EmailAddress]
-        [Required]
-        public string LoginName { get; set; } = string.Empty;
-
         [Required]
         public string UserRole { get; set; } = string.Empty;
     }
