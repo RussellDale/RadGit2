16bab1d7c9419559fffc0d2de5d7866061627dea
Russell Dale <russgm1@gmail.com>
2024-04-29 15:16:58 -0500

    new

--- Rad2/Data/UserData.cs
+++ Rad2/Data/UserData.cs
@@ -6,5 +6,11 @@ namespace Rad2.Data
     {
         [Required]
         public string UserRole { get; set; } = string.Empty;
+
+        public bool Create {  get; set; } = true; 
+        public bool Read { get; set; } = true;
+        public bool Update { get; set; } = true;
+        public bool Delete { get; set; } = true;
+        public bool Print { get; set; } = true;
     }
 }
