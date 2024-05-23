52ec30f1bf64eaecbd5d4ed4c98065540d04c24d
RussellDale <44413812+RussellDale@users.noreply.github.com>
2024-05-23 02:06:07 +0000

    New

---
+++ Rad2/Policy/NameHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Rad2.Policy
+{
+    public class NameHandler : AuthorizationHandler<NameRequirement>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, NameRequirement requirement)
+        {
+            Claim? name = null;
+
+            name = Handle(context.User, requirement, name);
+
+            if (name is null)
+                context.Fail();
+            else
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+
+        public Task HandleRequirementAsync2(AuthenticationState context, NameRequirement requirement)
+        {
+            Claim? name = null;
+
+            name = Handle(context.User, requirement, name);
+
+            return Task.CompletedTask;
+        }
+
+        private Claim? Handle(ClaimsPrincipal user, NameRequirement requirement, Claim? name)
+        {
+            name = user.FindFirst(c => c.Type == "Name");
+
+            if (name is not null)
+            {
+                requirement.Name = name.Value;
+            }
+
+            return name;
+        }
+    }
+}
---
+++ Rad2/Policy/NameRequirement.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Rad2.Policy
+{
+    public class NameRequirement : IAuthorizationRequirement
+    {
+        public NameRequirement(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
