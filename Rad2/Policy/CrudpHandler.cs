21bdf7cb122c9c84c8bab82e8dee8cf5ec1c0c3a
Russell Dale <russgm1@gmail.com>
2024-07-10 13:59:39 -0500

    new

--- Rad2/Policy/CrudpHandler.cs
+++ Rad2/Policy/CrudpHandler.cs
@@ -1,6 +1,10 @@
-﻿using System.Security.Claims;
+﻿using System;
+using System.Security.Claims;
+using DocumentFormat.OpenXml.InkML;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Rad2.Policy
 {
@@ -29,18 +33,37 @@ namespace Rad2.Policy
             return Task.CompletedTask;
         }
 
+        public Task HandleRequirementAsync3(IList<Claim> claims, CrudpRequirement requirement)
+        {
+            Claim? crudp = null;
+
+            crudp = Handle2(claims, requirement, crudp);
+
+            return Task.CompletedTask;
+        }
         private Claim? Handle(ClaimsPrincipal user, CrudpRequirement requirement, Claim? crudp)
         {
             crudp = user.FindFirst(c => c.Type == "CRUDP");
 
+            return Claim(crudp, requirement);
+        }
+        private Claim? Handle2(IList<Claim> claims, CrudpRequirement requirement, Claim? crudp)
+        {
+            crudp = claims.First(c => c.Type == "CRUDP");
+
+            return Claim(crudp, requirement);
+        }
+
+        private Claim? Claim(Claim? crudp, CrudpRequirement requirement)
+        {
             if (crudp is not null)
             {
                 if (crudp.Value.Substring(0, 1) == "0") requirement.Create = false;
-                if (crudp.Value.Substring(1, 1) == "0") requirement.Read = false;
+                if (crudp.Value.Substring(1, 1) == "0") requirement.Read   = false;
                 if (crudp.Value.Substring(2, 1) == "0") requirement.Update = false;
                 if (crudp.Value.Substring(3, 1) == "0") requirement.Delete = false;
-                if (crudp.Value.Substring(4, 1) == "0") requirement.Print = false;
-                if (crudp.Value.Substring(5, 1) == "0") requirement.Admin = false;
+                if (crudp.Value.Substring(4, 1) == "0") requirement.Print  = false;
+                if (crudp.Value.Substring(5, 1) == "0") requirement.Admin  = false;
             }
 
             return crudp;
