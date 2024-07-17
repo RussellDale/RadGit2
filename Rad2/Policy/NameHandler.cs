41af8f139c41f106636d3fd9ba238986dffe0103
Russell Dale <russgm1@gmail.com>
2024-07-17 11:37:52 -0500

    new

--- Rad2/Policy/NameHandler.cs
+++ Rad2/Policy/NameHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using DocumentFormat.OpenXml.Wordprocessing;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components.Authorization;
 
@@ -28,11 +29,31 @@ namespace Rad2.Policy
 
             return Task.CompletedTask;
         }
+        public Task HandleRequirementAsync3(IList<Claim> claims, NameRequirement requirement)
+        {
+            Claim? name = null;
 
+            name = Handle2(claims, requirement, name);
+
+            return Task.CompletedTask;
+        }
         private Claim? Handle(ClaimsPrincipal user, NameRequirement requirement, Claim? name)
         {
             name = user.FindFirst(c => c.Type == "Name");
 
+            return Claim(name, requirement);
+        }
+        private Claim? Handle2(IList<Claim> claims, NameRequirement requirement, Claim? name)
+        {
+            if (claims.Count == 0) { }
+            else
+                name = claims.First(c => c.Type == "Name");
+
+            return Claim(name, requirement);
+        }
+
+        private Claim? Claim(Claim? name, NameRequirement requirement)
+        {
             if (name is not null)
             {
                 requirement.Name = name.Value;
